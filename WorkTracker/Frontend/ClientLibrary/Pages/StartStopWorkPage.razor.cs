d25b0ff62fd6e3c7173fb3b573d4cf3f5044c8b5
Theodor Risager <theodor349@gmail.com>
2022-01-22 16:57:08 +0100

    Added a new Page

---
+++ WorkTracker/Frontend/ClientLibrary/Pages/StartStopWorkPage.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientLibrary.Pages
+{
+    public partial class StartStopWorkPage : ComponentBase
+    {
+        [Inject]
+        public HttpClient _client { get; set; }
+        public List<EmployerModel>? employers;
+
+        protected override async Task OnInitializedAsync()
+        {
+            employers = await _client.GetFromJsonAsync<List<EmployerModel>>("api/Employer");
+        }
+
+    }
+}
