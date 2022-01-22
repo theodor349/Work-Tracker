e831335fd072dc8a38ad25b58bd86403456a705d
Theodor Risager <theodor349@gmail.com>
2022-01-22 22:01:12 +0100

    Designed StartStop Time page

--- WorkTracker/Frontend/ClientLibrary/Pages/StartStopWorkPage.razor.cs
+++ WorkTracker/Frontend/ClientLibrary/Pages/StartStopWorkPage.razor.cs
@@ -13,12 +13,39 @@ namespace ClientLibrary.Pages
     {
         [Inject]
         public HttpClient _client { get; set; }
-        public List<EmployerModel>? employers;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public List<EmployerModelDTO>? Employers;
 
         protected override async Task OnInitializedAsync()
         {
-            employers = await _client.GetFromJsonAsync<List<EmployerModel>>("api/Employer");
+            var res = await _client.GetFromJsonAsync<List<EmployerModel>>("api/Employer");
+            Employers = res.ConvertAll(x => new EmployerModelDTO()
+            {
+                Name = x.Name,
+                StartTime = DateTime.Now.AddHours(-1),
+                TimeThisMonth = new TimeSpan(20,16,0),
+            });
+
+            Employers.Add(new EmployerModelDTO()
+            {
+                Name = "Some other corporation",
+                StartTime = null,
+                TimeThisMonth = new TimeSpan(2, 46, 12),
+            });
         }
 
+        private void ResetDate()
+        {
+            Date = DateTime.Now;
+            Console.WriteLine("Reset Date");
+        }
+    }
+
+    public class EmployerModelDTO
+    {
+        public string Name { get; set; }
+        public DateTime? StartTime { get; set; }
+        public TimeSpan TimeThisMonth { get; set; }
+        public bool IsStarted => StartTime is not null;
     }
 }
